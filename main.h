1eb34809a4c0dcf38696de410b9526ace4b5e507
zeRO00999 <zeronine700@gmail.com>
2023-10-16 12:17:44 -0700

    000

--- main.h
+++ main.h
@@ -1,33 +1,40 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define INT_BITS (4 * 8)
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
 /**
- * struct format - Struct for format
- * @specifiers: Struct format
- * @f: The function associated
+ * struct call - struct call
+ * @t: t - flag for data type
+ * @f: function - assocated function
+ *
  */
-
-typedef struct specifiers
+typedef struct call
 {
-	char specifiers;
-	int (*f)(va_list);
-} specifiers_t;
+	char t;
+	int (*f)(char *, va_list, int);
+} call_t;
 
-/*prototypes*/
 int _printf(const char *format, ...);
-int get_function(char s, va_list args);
-int _putchar(char c);
-
-/*Conversion specifiers*/
-int print_char(va_list args);
-int print_string(va_list args);
-int print_digit(va_list args);
-int print_mod(va_list args);
-int print_rev_string(va_list args);
+int buff_append(char *buff_dest, va_list arg, int buff_count, char type);
+int print_buff(char *buff, unsigned int nbuff);
+int str_len(char *s);
+char *_strcpy(char *dest, char *src);
+int parse_char(char *buff_dest, va_list arg, int buff_count);
+int parse_str(char *buff_dest, va_list arg, int buff_count);
+int parse_int(char *buff_dest, va_list list, int buff_count);
+int parse_perc(char *buff_dest, va_list arg, int buff_count);
+int parse_bin(char *buff_dest, va_list arg, int buff_count);
+int parse_oct(char *buff_dest, va_list arg, int buff_count);
+int parse_hex(char *buff_dest, va_list arg, int buff_count);
+int parse_X(char *buff_dest, va_list arg, int buff_count);
+int parse_uint(char *buff_dest, va_list arg, int buff_count);
+int parse_rev(char *buff_dest, va_list arg, int buff_count);
+int parse_R13(char *buff_dest, va_list arg, int buff_count);
 
 #endif
