044666b0a4c27a7b5c4778c22058dbd06ef0b31e
zeRO00999 <zeronine700@gmail.com>
2023-10-16 14:29:17 -0700

    00

--- main.c
+++ main.c
@@ -12,34 +12,38 @@ int main(void)
 	int len;
 	int len2;
 	unsigned int ui;
-	void *addr;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
+	len = _printf("Should print a single percent sign: %%\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	ui = (unsigned int)INT_MAX + 1024;
-	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
-	_printf("Unsigned octal:[%o]\n", ui);
-	printf("Unsigned octal:[%o]\n", ui);
-	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
 	_printf("Character:[%c]\n", 'H');
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
-	_printf("Address:[%p]\n", addr);
-	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	/**_printf("Unknown:[%r]\n");
-	printf("Unknown:[%r]\n");*/
+	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
+_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+_printf("%s", "This sentence is retrieved from va_args!\n");
+_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char*)0);
+_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+_printf("%%");
+_printf("Should print a single percent sign: %%\n");
+_printf("css%ccs%scscscs", 'T', "Test");
+_printf("%!\n");
+_printf("%K\n");
+len = _printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	len2 = printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	fflush(stdout);
 
 	return (0);
 }
